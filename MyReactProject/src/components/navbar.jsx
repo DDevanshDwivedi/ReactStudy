0573c4cd7b83e1a71c3d0a7571c7ad39a1a31923
DDevanshDwivedi <devanshdwivedi605@gmail.com>
2025-04-14 11:32:23 +0530

    Introduction to react

---
+++ MyReactProject/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        < div >
+        <div>
+            This is Navbar
+        </div>
+        </div >
+    )
+}
+export default Navbar;
